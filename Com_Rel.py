a87eecfa072b91db1dfeea98522f227d54d5023c
Omid-Ghab <126298295+Omid-Ghab@users.noreply.github.com>
2023-03-21 12:24:49 -0700

    Update Com_Rel.py

--- Com_Rel.py
+++ Com_Rel.py
@@ -11,8 +11,7 @@ from reliability.Repairable_systems import optimal_replacement_time
 from reliability.Probability_plotting import plot_points
 from reliability.Fitters import Fit_Everything
 from reliability.Other_functions import make_right_censored_data
-import tk
-import re
+import tkinter as tkimport re
 import matplotlib
 matplotlib.use('tkAgg')
 import matplotlib.pyplot as plt
