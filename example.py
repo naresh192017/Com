d4ec8eb81b6ac4632c8db21a879b800d898ec7f8
Naresh Bhat <63491176+naresh192017@users.noreply.github.com>
2023-03-26 11:06:25 +0530

    Update example.py

--- example.py
+++ example.py
@@ -2,38 +2,22 @@ import streamlit as st
 
 def home():
     st.title("Home Page")
-    option = st.radio(
-        "Select an option:",
-        ("Perform basic calculations", "Perform advanced calculations")
-    )
-    if option == "Perform basic calculations":
-        basic_calculations()
-    elif option == "Perform advanced calculations":
-        advanced_calculations()
+    st.write("Welcome to the home page")
 
-def basic_calculations():
-    st.title("Basic Calculations")
-    operation = st.sidebar.selectbox(
-        "Select an operation",
-        ("Addition", "Subtraction", "Multiplication", "Division")
-    )
-    x = st.sidebar.number_input("Enter a value for x:", value=0)
-    y = st.sidebar.number_input("Enter a value for y:", value=0)
-    if operation == "Addition":
-        result = x + y
-    elif operation == "Subtraction":
-        result = x - y
-    elif operation == "Multiplication":
-        result = x * y
-    elif operation == "Division":
-        result = x / y
-    else:
-        result = None
-    if result is not None:
-        st.write(f"Result: {result}")
+def about():
+    st.title("About Page")
+    st.write("This is the about page")
 
-def advanced_calculations():
-    st.title("Advanced Calculations")
-    # TODO: Implement advanced calculations
+# Create a dictionary to map page names to functions
+pages = {
+    "Home": home,
+    "About": about
+}
 
-home()
+# Add a menu to the sidebar to allow the user to switch between pages
+st.sidebar.title("Navigation")
+selection = st.sidebar.radio("Go to", list(pages.keys()))
+
+# Call the selected page function
+page = pages[selection]
+page()
