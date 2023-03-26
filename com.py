f8ccf0f86252b51a311d728636c84127df080d1c
Naresh Bhat <63491176+naresh192017@users.noreply.github.com>
2023-03-26 09:56:26 +0530

    Update com.py

--- com.py
+++ com.py
@@ -339,7 +339,10 @@ def show_assistant_comp_config_File():
             st.write('File ', comp_config_filename, ' was saved.')
 
 #=================================
-tab_comp, tab_conf, tab_montc, tab_sen = st.sidebar.tabs(['Components Definition', 'Configuration Definition', 'Monte Carlo Calculation','Sensitivity Analysis'])
+st.title("Component Reliability")
+comp_rel, tab_comp, tab_conf, tab_montc, tab_sen = st.sidebar.tabs(['Component Reliability','Components Definition', 'Configuration Definition', 'Monte Carlo Calculation','Sensitivity Analysis'])
+   
+    
 with tab_comp:
     st.write('Steps for defining a componet:  \n1- Click on the Enter your component key.  \n2- Click on Add New Component key.    \n3- Type a unique name for your component.     \n4- Choose the component distribution from the list.     \n5- Enter your Parameters.      \n6- Click on Add/Edit componet.       \n7- Clike on Edit button for editing an existing component.')
     st.button('Enter your Components ', on_click=show_comp_def_File)
