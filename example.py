3e43815c97fb5c3975c48eeae93aca24d89d2571
Naresh Bhat <63491176+naresh192017@users.noreply.github.com>
2023-03-26 09:42:07 +0530

    Update example.py

--- example.py
+++ example.py
@@ -3,19 +3,24 @@ import streamlit as st
 # Define the home page
 def home():
     st.title("Home Page")
-    st.write("Welcome to my Streamlit app!")
-    st.write("Use the navigation bar to access other pages.")
+    choice = st.radio(
+        "Select an option:",
+        ["Perform basic calculations", "Perform advanced calculations"]
+    )
+    if choice == "Perform basic calculations":
+        basic_calculations()
+    elif choice == "Perform advanced calculations":
+        advanced_calculations()
 
-# Define the calculations page
-def calculations():
-    st.title("Calculations Page")
-    st.write("Enter the numbers you'd like to calculate:")
-    x = st.number_input("Enter a value for x:")
-    y = st.number_input("Enter a value for y:")
-    operation = st.selectbox(
+# Define the basic calculations page
+def basic_calculations():
+    st.title("Basic Calculations")
+    operation = st.sidebar.selectbox(
         "Select an operation",
         ["Addition", "Subtraction", "Multiplication", "Division"]
     )
+    x = st.sidebar.number_input("Enter a value for x:")
+    y = st.sidebar.number_input("Enter a value for y:")
     if operation == "Addition":
         result = x + y
     elif operation == "Subtraction":
@@ -26,12 +31,11 @@ def calculations():
         result = x / y
     st.write(f"Result: {result}")
 
-# Define the navigation bar
-pages = {
-    "Home": home,
-    "Calculations": calculations
-}
-navigation = st.sidebar.radio("Go to", list(pages.keys()))
+# Define the advanced calculations page
+def advanced_calculations():
+    st.title("Advanced Calculations")
+    # TODO: Implement more complex calculations here
+    st.write("Sorry, this feature is still under construction.")
 
-# Display the selected page
-pages[navigation]()
+# Run the app
+home()
